623b7baf84bc3e9f00044f317766db3984dc43b0
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-09-29 14:39:03 +0300

    util: middleware error handling

--- blog-app/index.js
+++ blog-app/index.js
@@ -3,25 +3,33 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
+    .then(() => {
+        logger.info('connected to mongo db')
+    })
+    .catch(error => {
+        logger.error('error connecting to mongo:', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
+app.get('/api/blogs', (request, response, next) => {
     Blog
         .find({})
         .then(blogs => {
             response.json(blogs)
         })
+        .catch(error => next(error))
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
     const body = request.body
 
     const blog = new Blog({
@@ -36,7 +44,10 @@ app.post('/api/blogs', (request, response) => {
         .then(result => {
             response.status(201).json(result)
         })
+        .catch(error => next(error))
 })
+
+app.use(middleware.errorHandler)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
