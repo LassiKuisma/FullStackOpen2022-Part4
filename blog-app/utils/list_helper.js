50c629148aaaa4456f506c0664c6016cc59724a8
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-09-30 01:12:36 +0300

    exercise 4.5

--- blog-app/utils/list_helper.js
+++ blog-app/utils/list_helper.js
@@ -8,7 +8,35 @@ const totalLikes = (blogs) => {
         .reduce((previousValue, likes) => previousValue + likes, 0)
 }
 
+const mostLiked = (blogs) => {
+    if (blogs.length === 0) {
+        return undefined
+    }
+
+    const highestLikes = (previousValue, blog) => {
+        if (previousValue === null) {
+            return blog
+        }
+
+        if (blog.likes > previousValue.likes) {
+            return blog
+        }
+
+        return previousValue
+    }
+
+    const highest = blogs.reduce(highestLikes, null)
+
+    return {
+        title: highest.title,
+        author: highest.author,
+        likes: highest.likes,
+    }
+
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    mostLiked,
 }
