89be7da97d7172db2c95463b168b0da228c894b5
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-09-30 00:54:00 +0300

    exercise 4.4

--- blog-app/utils/list_helper.js
+++ blog-app/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs
+        .map(blog => blog.likes)
+        .reduce((previousValue, likes) => previousValue + likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
