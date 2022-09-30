92196fa099f28699f922f1fa5678125b37831d67
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-09-30 23:39:47 +0300

    example: use for-of in beforeEach

--- examples/notes/tests/note_api.test.js
+++ examples/notes/tests/note_api.test.js
@@ -8,10 +8,11 @@ const Note = require('../models/note')
 
 beforeEach(async () => {
     await Note.deleteMany({})
-    let noteObjects = helper.initialNotes
-        .map(note => new Note(note))
-    const promiseArray = noteObjects.map(note => note.save())
-    await Promise.all(promiseArray)
+
+    for (let note of helper.initialNotes) {
+        let noteObject = new Note(note)
+        await noteObject.save()
+    }
 })
 
 test('notes are returned as json', async () => {
