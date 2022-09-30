e7ab2003eb20a85acf787bcdd198723524579847
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-09-30 23:06:02 +0300

    example: async errors

--- examples/notes/controllers/notes.js
+++ examples/notes/controllers/notes.js
@@ -6,21 +6,16 @@ notesRouter.get('/', async (_request, response) => {
     response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
-    try {
-        const note = await Note.findById(request.params.id)
-        if (note) {
-            response.json(note)
-        } else {
-            response.status(404).end()
-        }
-
-    } catch (exception) {
-        next(exception)
+notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
     }
 })
 
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
 
     const note = new Note({
@@ -29,21 +24,13 @@ notesRouter.post('/', async (request, response, next) => {
         date: new Date(),
     })
 
-    try {
-        const saved = await note.save()
-        response.status(201).json(saved)
-    } catch (exception) {
-        next(exception)
-    }
+    const saved = await note.save()
+    response.status(201).json(saved)
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-    try {
-        await Note.findByIdAndRemove(request.params.id)
-        response.status(204).end()
-    } catch (exception) {
-        next(exception)
-    }
+notesRouter.delete('/:id', async (request, response) => {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
 notesRouter.put('/:id', (request, response, next) => {
