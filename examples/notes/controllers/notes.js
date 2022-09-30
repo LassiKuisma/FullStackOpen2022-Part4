048fc197c37bcb54f5b04366dec0914dc1fb39bd
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-09-30 23:00:52 +0300

    example: make router use async/await

--- examples/notes/controllers/notes.js
+++ examples/notes/controllers/notes.js
@@ -6,19 +6,21 @@ notesRouter.get('/', async (_request, response) => {
     response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+    try {
+        const note = await Note.findById(request.params.id)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+
+    } catch (exception) {
+        next(exception)
+    }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
     const body = request.body
 
     const note = new Note({
@@ -27,19 +29,21 @@ notesRouter.post('/', (request, response, next) => {
         date: new Date(),
     })
 
-    note.save()
-        .then(saved => {
-            response.status(201).json(saved)
-        })
-        .catch(error => next(error))
+    try {
+        const saved = await note.save()
+        response.status(201).json(saved)
+    } catch (exception) {
+        next(exception)
+    }
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+    try {
+        await Note.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch (exception) {
+        next(exception)
+    }
 })
 
 notesRouter.put('/:id', (request, response, next) => {
