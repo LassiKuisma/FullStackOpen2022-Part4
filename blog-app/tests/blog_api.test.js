ede5f699894f5aebb87c6385e10cd7be06fc84e7
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-10-12 22:19:18 +0300

    exercise 4.9

--- blog-app/tests/blog_api.test.js
+++ blog-app/tests/blog_api.test.js
@@ -38,6 +38,13 @@ test('a specific blogs is within the returned blogs', async () => {
     )
 })
 
+test('blogs have unique id', async () => {
+    const response = await api.get('/api/blogs')
+
+    const first = response.body[0]
+    expect(first.id).toBeDefined()
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
