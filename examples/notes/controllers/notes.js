765ce12ffb3b597e7f072c8d5f50c228c6226cdc
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-10-13 19:34:22 +0300

    example: authenticate with tokens

--- examples/notes/controllers/notes.js
+++ examples/notes/controllers/notes.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 notesRouter.get('/', async (_request, response) => {
     const notes = await Note
@@ -20,8 +29,13 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
     const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
-    const user = await User.findById(body.userId)
+    const user = await User.findById(decodedToken.id)
 
     const note = new Note({
         content: body.content,
@@ -34,7 +48,7 @@ notesRouter.post('/', async (request, response) => {
     user.notes = user.notes.concat(saved._id)
     await user.save()
 
-    response.status(201).json(saved)
+    response.json(saved)
 })
 
 notesRouter.delete('/:id', async (request, response) => {
