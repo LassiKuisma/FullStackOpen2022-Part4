3e9778e02d26701796948b918345eb5703d8b836
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-10-13 22:02:14 +0300

    exercise 4.16

--- blog-app/controllers/users.js
+++ blog-app/controllers/users.js
@@ -5,6 +5,37 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
 
+    if (!username) {
+        response.status(400).json({
+            error: 'missing username'
+        })
+    }
+
+    if (!password) {
+        response.status(400).json({
+            error: 'missing password'
+        })
+    }
+
+    if (username.length < 3) {
+        response.status(400).json({
+            error: 'username must be over 3 characters'
+        })
+    }
+
+    if (password.length < 3) {
+        response.status(400).json({
+            error: 'password must be over 3 characters'
+        })
+    }
+
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+        response.status(400).json({
+            error: 'username is taken'
+        })
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
--- blog-app/tests/blog_api.test.js
+++ blog-app/tests/blog_api.test.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -163,6 +165,146 @@ test('trying to modify blog post that doesnt exist gives error', async () => {
     const blogsAtEnd = await helper.blogsInDb()
     console.log(blogsAtEnd)
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('initial guys password', 10)
+        const user = new User({
+            username: 'first',
+            name: 'Guy First',
+            passwordHash,
+        })
+
+        await user.save()
+    })
+
+    test('new user with unique username can be added', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'second',
+            name: 'Guy First',
+            password: 'asddsaqweewq',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+
+    test('creation fails if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'first',
+            name: 'Uvuvwevwevwe Ossas',
+            password: 'asddsaqweewq',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('username is taken')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
+
+    test('creation fails if username not given', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            name: 'Uvuvwevwevwe Ossas',
+            password: 'asddsaqweewq',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('missing username')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
+
+    test('creation fails if username too short', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'sn',
+            name: 'Mr Short Name',
+            password: 'aaabbbccc',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('username must be over 3 characters')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
+
+    test('creation fails if password too short', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'uniquename',
+            name: 'Mr long pword',
+            password: 'a',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('password must be over 3 characters')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
+
+    test('creation fails if password not given', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'nopword',
+            name: 'I dont need passwords',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('missing password')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
 })
 
 afterAll(() => {
