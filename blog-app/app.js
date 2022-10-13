35def66bdc23c4eecde414b28b11c85dedd206e7
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-10-13 21:20:44 +0300

    exercise 4.15

--- blog-app/app.js
+++ blog-app/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -26,6 +27,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
