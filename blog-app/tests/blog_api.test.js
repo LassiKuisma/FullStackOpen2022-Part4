0b382d4b9f2775663f82c0724ffea5506001078a
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-10-14 16:37:37 +0300

    refactor tests
    Move some tests inside describe-block, and rename some tests

--- blog-app/tests/blog_api.test.js
+++ blog-app/tests/blog_api.test.js
@@ -17,154 +17,155 @@ beforeEach(async () => {
     }
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
+describe('when there are initially some blogs in db', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
 
-test('corrent number of blogs is returned', async () => {
-    const response = await api.get('/api/blogs')
+    test('corrent number of blogs is returned', async () => {
+        const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
 
-test('a specific blogs is within the returned blogs', async () => {
-    const response = await api.get('/api/blogs')
+    test('a specific blogs is within the returned blogs', async () => {
+        const response = await api.get('/api/blogs')
 
-    const responseTitles = response.body.map(blog => blog.title)
+        const responseTitles = response.body.map(blog => blog.title)
 
-    expect(responseTitles).toContain(
-        'Another test in the blog'
-    )
-})
+        expect(responseTitles).toContain(
+            'Another test in the blog'
+        )
+    })
 
-test('blogs have unique id', async () => {
-    const response = await api.get('/api/blogs')
+    test('blogs have a defined id', async () => {
+        const response = await api.get('/api/blogs')
 
-    const first = response.body[0]
-    expect(first.id).toBeDefined()
-})
+        const first = response.body[0]
+        expect(first.id).toBeDefined()
+    })
 
-test('new blog can be posted', async () => {
-    const newBlog = {
-        title: 'Newly added blog',
-        author: 'Person Person',
-        url: 'url #2',
-        likes: 1,
-    }
+    test('new blog can be posted', async () => {
+        const newBlog = {
+            title: 'Newly added blog',
+            author: 'Person Person',
+            url: 'url #2',
+            likes: 1,
+        }
 
-    await api.post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-    const contents = blogsAtEnd.map(b => b.title)
-    expect(contents).toContain('Newly added blog')
-})
+        const contents = blogsAtEnd.map(b => b.title)
+        expect(contents).toContain('Newly added blog')
+    })
 
-test('posting a blog without specifying amount of likes creates new blog with zero likes', async () => {
-    const newBlog = {
-        title: 'A blog with no likes',
-        author: 'Dewey',
-        url: 'url #3',
-    }
+    test('posting a blog without specifying amount of likes creates new blog with zero likes', async () => {
+        const newBlog = {
+            title: 'A blog with no likes',
+            author: 'Dewey',
+            url: 'url #3',
+        }
 
-    await api.post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    const blog = blogsAtEnd.find(blog => blog.title === 'A blog with no likes')
+        const blogsAtEnd = await helper.blogsInDb()
+        const blog = blogsAtEnd.find(blog => blog.title === 'A blog with no likes')
 
-    expect(blog.likes).toBe(0)
-})
+        expect(blog.likes).toBe(0)
+    })
 
-test('trying to post a blog without title gives status 400', async () => {
-    const newBlog = {
-        author: 'Missing Title',
-        url: 'url #4',
-        likes: 777,
-    }
+    test('posting a blog without title fails with statuscode and message', async () => {
+        const newBlog = {
+            author: 'Missing Title',
+            url: 'url #4',
+            likes: 777,
+        }
 
-    await api.post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-})
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
 
-test('trying to post a blog without author gives status 400', async () => {
-    const newBlog = {
-        title: 'Title is present, but author nowehere to be seen',
-        url: 'url #5',
-        likes: 123,
-    }
+    test('posting a blog without author fails with statuscode and message', async () => {
+        const newBlog = {
+            title: 'Title is present, but author nowehere to be seen',
+            url: 'url #5',
+            likes: 123,
+        }
 
-    await api.post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-})
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
 
-test('blog posts can be deleted', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
-    const id = blogToDelete.id
+    test('blog posts can be deleted', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+        const id = blogToDelete.id
 
-    await api.delete(`/api/blogs/${id}`)
-        .expect(204)
+        await api.delete(`/api/blogs/${id}`)
+            .expect(204)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
 
-    const titles = blogsAtEnd.map(blog => blog.title)
-    expect(titles).not.toContain(blogToDelete.title)
-})
+        const titles = blogsAtEnd.map(blog => blog.title)
+        expect(titles).not.toContain(blogToDelete.title)
+    })
 
-test('trying to delete blog post that doesnt exist gives status 404', async () => {
-    const id = 123456
+    test('deleting blog post that doesnt exist fails with statuscode and message', async () => {
+        const id = 123456
 
-    await api.delete(`/api/blogs/${id}`)
-        .expect(400)
-})
+        await api.delete(`/api/blogs/${id}`)
+            .expect(400)
+    })
 
-test('blog posts can be modified', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToModify = blogsAtStart[0]
-    const id = blogToModify.id
+    test('blog posts can be modified', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToModify = blogsAtStart[0]
+        const id = blogToModify.id
 
-    const newBlog = {
-        likes: 10000,
-    }
+        const newBlog = {
+            likes: 10000,
+        }
 
-    await api.put(`/api/blogs/${id}`)
-        .send(newBlog)
-        .expect(200)
+        await api.put(`/api/blogs/${id}`)
+            .send(newBlog)
+            .expect(200)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 
-    const updatedBlog = blogsAtEnd.find(blog => blog.title === 'Test blog #1')
-    expect(updatedBlog.likes).toBe(10000)
-})
+        const updatedBlog = blogsAtEnd.find(blog => blog.title === 'Test blog #1')
+        expect(updatedBlog.likes).toBe(10000)
+    })
 
-test('trying to modify blog post that doesnt exist gives error', async () => {
-    const id = 123321
+    test('modifying blog post that doesnt exist fails with statuscode and message', async () => {
+        const id = 123321
 
-    const newBlog = {
-        likes: 10000,
-    }
+        const newBlog = {
+            likes: 10000,
+        }
 
-    await api.put(`/api/blogs/${id}`)
-        .send(newBlog)
-        .expect(400)
+        await api.put(`/api/blogs/${id}`)
+            .send(newBlog)
+            .expect(400)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    console.log(blogsAtEnd)
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
 })
 
 describe('when there is initially one user in db', () => {
